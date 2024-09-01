7ece018a798fc45b06c56f859d28b275d172627a
matias-oggero <matias-oggero@hotmail.com>
2024-09-01 20:31:44 -0300

    crud servicios maquetacion

--- src/app/components/create-servicio/create-servicio.component.ts
+++ src/app/components/create-servicio/create-servicio.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-servicio',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatButtonModule,MatInputModule, MatDialogModule],
   templateUrl: './create-servicio.component.html',
   styleUrl: './create-servicio.component.scss'
 })
--- src/app/components/edit-servicio/edit-servicio.component.ts
+++ src/app/components/edit-servicio/edit-servicio.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-edit-servicio',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatButtonModule,MatInputModule, MatDialogModule],
   templateUrl: './edit-servicio.component.html',
   styleUrl: './edit-servicio.component.scss'
 })
