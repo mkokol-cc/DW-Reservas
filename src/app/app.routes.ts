b0fc89d7988edc7f87d2bc546ce39c0226e651fc
matias-oggero <matias-oggero@hotmail.com>
2024-09-01 20:54:01 -0300

    in dashboard add toolbar y sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ListServicioComponent } from './components/list-servicio/list-servicio.component';
 
 export const routes: Routes = [
 
+
+    {path: '', component: DashboardComponent, children:[
+
+        {path: 'servicios', loadComponent: () => import('./components/list-servicio/list-servicio.component').then(m => m.ListServicioComponent), pathMatch: 'full'},
+    ]},
+
+    /*
     {
         path: 'prueba',
         loadComponent: () => import('./components/list-servicio/list-servicio.component').then(m => m.ListServicioComponent)
-    },
+    },*/
 
 ];
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,RouterOutlet,MatSidenavModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
