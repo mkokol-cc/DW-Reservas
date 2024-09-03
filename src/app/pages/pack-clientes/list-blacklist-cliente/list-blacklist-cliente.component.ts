964e355b74415c7851748f0bd882f356dafcf4c3
matias-oggero <matias-oggero@hotmail.com>
2024-09-03 15:53:21 -0300

    add clientes

---
+++ src/app/pages/pack-clientes/list-blacklist-cliente/list-blacklist-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-blacklist-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-blacklist-cliente.component.html',
+  styleUrl: './list-blacklist-cliente.component.scss'
+})
+export class ListBlacklistClienteComponent {
+
+}
---
+++ src/app/pages/pack-clientes/list-cliente/list-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-cliente.component.html',
+  styleUrl: './list-cliente.component.scss'
+})
+export class ListClienteComponent {
+
+}
