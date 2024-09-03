f5f55eb1dcbc9c2774abe808a2ad3a04c5f1c151
matias-oggero <matias-oggero@hotmail.com>
2024-09-03 15:13:12 -0300

    create sidenav-menu

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,11 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { SidenavMenuComponent } from '../../components/sidenav-menu/sidenav-menu.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule,RouterOutlet,MatSidenavModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,RouterOutlet,MatSidenavModule, SidenavMenuComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
