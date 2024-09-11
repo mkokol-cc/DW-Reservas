19859c58630bdf4e3d9f8f082f9a8ffbe66eb7d9
matias-oggero <matias-oggero@hotmail.com>
2024-09-10 23:21:34 -0300

    add style in buzon and perfil menu

--- src/app/components/buzon-menu/buzon-menu.component.ts
+++ src/app/components/buzon-menu/buzon-menu.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-buzon-menu',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule,MatBadgeModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule,MatBadgeModule,
+    MatDividerModule],
   templateUrl: './buzon-menu.component.html',
   styleUrl: './buzon-menu.component.scss'
 })
--- src/app/components/perfil-menu/perfil-menu.component.ts
+++ src/app/components/perfil-menu/perfil-menu.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-perfil-menu',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule,MatBadgeModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule,MatBadgeModule,MatDividerModule],
   templateUrl: './perfil-menu.component.html',
   styleUrl: './perfil-menu.component.scss'
 })
