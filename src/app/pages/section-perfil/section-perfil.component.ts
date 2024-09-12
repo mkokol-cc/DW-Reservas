5144ac503d1165d4da2561ff330c8a5a29deb8f0
matias-oggero <matias-oggero@hotmail.com>
2024-09-12 00:11:47 -0300

    fix: perfil-section

--- src/app/pages/section-perfil/section-perfil.component.ts
+++ src/app/pages/section-perfil/section-perfil.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-section-perfil',
   standalone: true,
-  imports: [MatDividerModule,MatCardModule],
+  imports: [MatDividerModule,MatCardModule,MatFormFieldModule, MatInputModule,MatListModule,MatIconModule],
   templateUrl: './section-perfil.component.html',
   styleUrl: './section-perfil.component.scss'
 })
